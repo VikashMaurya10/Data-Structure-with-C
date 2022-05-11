d1396971465ae122269920615793e041aa1f18cd
Vikash <vikashmaurya10stp@gmail.com>
2022-05-11 23:22:17 +0530

    Commit

--- Linked_List/p4.c
+++ Linked_List/p4.c
@@ -8,6 +8,8 @@ void number_of_nodes();
 void insert_node_end();
 void insert_node_after_position();
 void delete_node_at_potion();
+void delete_node_at_begining();
+void delete_node_at_end();
 struct node
 {
     int data;
@@ -16,12 +18,13 @@ struct node
 struct node *head, *new_node, *temp, *temp1;
 void main()
 {
+    system("cls");
     int choice;
     do
     {
-        Sleep(2000);
+        Sleep(1800);
         printf("\n******** MANU ********");
-        printf("\n1. Create Linked List\n2. Display the Linked List\n3. Display the number of node\n4. Insert node Beginning\n5. Insert node at End\n6. Insert node after the position\n7. Delete node at Potion\n8. To Exit\n");
+        printf("\n1. Create Linked List\n2. Display the Linked List\n3. Display the number of node\n4. Insert node Beginning\n5. Insert node at End\n6. Insert node after the position\n7. Delete node at Position\n8. Delete node at Begining\n9. Delete node at end\n10. To Exit\n");
         printf("\nEnter Your Option Number: ");
         scanf("%d", &choice);
         switch (choice)
@@ -48,13 +51,19 @@ void main()
             delete_node_at_potion();
             break;
         case 8:
-            printf("\nExited...");
+            delete_node_at_begining();
+            break;
+        case 9:
+            delete_node_at_end();
+            break;
+        case 10:
+            printf("\n-----Exited!-----\n");
             break;
         default:
             printf("\nPlease choose default option!..\n");
             break;
         }
-    } while (choice != 8);
+    } while (choice != 10);
 }
 void create_node()
 {
@@ -80,12 +89,13 @@ void create_node()
         printf("Enter the element of Linked List: ");
         scanf("%d", &i);
     }
+    printf("\nThe Linked List is created!..\n");
 }
 void display_linked_list()
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
@@ -103,11 +113,10 @@ void number_of_nodes()
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
-
         int count = 0;
         temp = head;
         while (temp != NULL)
@@ -123,7 +132,7 @@ void insert_in_begining()
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
@@ -140,14 +149,14 @@ void insert_in_begining()
             printf("Enter the element of Linked List: ");
             scanf("%d", &i);
         }
+        printf("\nThe node is Inserted at the Begining!...\n");
     }
-    printf("\n");
 }
 void insert_node_end()
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
@@ -163,18 +172,18 @@ void insert_node_end()
             temp = temp->link;
         }
         temp->link = new_node;
+        printf("\nThe node is inserted at the end!...\n");
     }
-    printf("\n");
 }
 void insert_node_after_position()
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
-        int i = 0,index;
+        int i = 0, index;
         printf("Enter the Insertion Position Number: ");
         scanf("%d", &index);
         new_node = (struct node *)malloc(sizeof(new_node));
@@ -188,28 +197,73 @@ void insert_node_after_position()
         }
         new_node->link = temp->link;
         temp->link = new_node;
+        printf("\nThe node is Inserted!..\n");
     }
-    printf("\n");
 }
 void delete_node_at_potion()
+
 {
     if (head == NULL)
     {
-        printf("The Linked list is empty!...");
+        printf("\nThe Linked list is empty!...\n");
     }
     else
     {
         int i = 0, index;
-        temp = head;
         printf("Enter the Insertion Position Number: ");
         scanf("%d", &index);
-        while (i != (index - 1))
+        if (index == 1)
         {
-            temp1 = temp;
+            delete_node_at_begining();
+        }
+        else
+        {
+            temp = head;
+            while (i != (index - 1))
+            {
+                temp1 = temp;
+                temp = temp->link;
+                i++;
+            }
+            temp1->link = temp->link;
+            printf("\nThe given position node is Deleted!....\n");
+        }
+    }
+}
+void delete_node_at_begining()
+{
+    if (head == NULL)
+    {
+        printf("\nThe Linked list is empty!...\n");
+    }
+    else
+    {
+        head = head->link;
+        printf("\nThe First Node is Deleted!....\n");
+    }
+}
+void delete_node_at_end()
+{
+    if (head == NULL)
+    {
+        printf("\nThe Linked list is empty!...\n");
+    }
+    else
+    {
+        int i = 0, count = 0, no_nodes;
+        temp = head;
+        while (temp != NULL)
+        {
+            count++;
             temp = temp->link;
+        }
+        temp1 = head;
+        while (i < (count - 2))
+        {
+            temp1 = temp1->link;
             i++;
         }
-        temp1->link = temp->link;
-        printf("Deleted!....\n");
+        temp1->link = NULL;
+        printf("\nThe Last Node is Deleted!....\n");
     }
 }
